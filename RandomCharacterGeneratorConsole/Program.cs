f1cf6008f20eae49635095d6271acc97071f7169
RBKJobs <bharanikrishnan93@outlook.com>
2024-01-20 12:32:46 +0530

    Add project files.

---
+++ RandomCharacterGeneratorConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using RandomCharacterGeneratorLibrary;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string password = "";
+
+        foreach (var item in new Random().GenerateRandomCharacters(16))
+        {
+            password += item;
+        }
+
+        Console.WriteLine(password);
+        Console.ReadLine();
+    }
+}
---
+++ RandomCharacterGeneratorLibrary/RandomCharacterExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCharacterGeneratorLibrary
+{
+    public static class RandomCharacterExtension
+    {
+        /// <summary>
+        /// Generate random characters based on given length.
+        /// </summary>
+        /// <param name="random">Random class extension.</param>
+        /// <param name="minLength">Minimum character length. Default character is 1</param>
+        /// <returns></returns>
+        public static IEnumerable<char> GenerateRandomCharacters(this Random random, int minLength = 1)
+        {
+            List<char> characters = new List<char>();
+
+            for (char c = 'A'; c <= 'z' ; c++)
+            {
+                characters.Add(c);
+            }
+
+            for(int i = 0; i < minLength; i++)
+            {
+                yield return characters[random.Next(characters.Count)];
+            }
+        }
+    }
+}
